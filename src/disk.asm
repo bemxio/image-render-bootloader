1d225e696f03d799d25fc7ddece83ac3ab49caf6
bemxio <bemxiov@protonmail.com>
2023-05-26 19:11:24 +0200

    improve disk read error  handling

--- src/disk.asm
+++ src/disk.asm
@@ -23,6 +23,7 @@ read_image:
 
 disk_error:
     mov bx, DISK_ERROR ; load the address of the error message
+    mov cl, ah ; load the error code into the `cl` register
 
     mov ah, 0x00 ; 'Set Video Mode' function
     mov al, 0x03 ; 80x25 text mode
@@ -30,11 +31,12 @@ disk_error:
     int 0x10 ; call the BIOS interrupt
 
     call print ; print the error message
+    call print_hex ; print the error code in hex
     call line_break ; add a line break
 
     hlt ; halt the system
 
 IMAGE_SIZE equ 125 ; 125 sectors (64,000 bytes) per image
 
-DISK_ERROR: db "Disk read error", 0
-;SECTOR_ERROR: db "Incorrect number of sectors read", 0
+DISK_ERROR: db "error: disk read failed with code 0x", 0
+;SECTOR_ERROR: db "error: incorrect number of sectors read", 0
