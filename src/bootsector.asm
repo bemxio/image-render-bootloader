acb90837f455ef78b05c05551af4d249a04114ed
bemxio <bemxiov@protonmail.com>
2025-07-04 16:20:47 +0200

    remove read_image function

--- src/bootsector.asm
+++ src/bootsector.asm
@@ -4,30 +4,23 @@
 mov ax, 0x13 ; "Set Video Mode" function with 320x200 color mode
 int 0x10 ; call the BIOS interrupt
 
-mov ax, 0xa000 ; set the video memory address
-mov es, ax ; move the address into the extra segment register
+mov ax, VIDEO_MEMORY_SEGMENT ; set the video memory segment
+mov es, ax ; move the segment into the extra segment register
+xor di, di ; clear the destination index
 
-call read_image ; read the image into the video memory
+mov ah, 0x02 ; "Read Sectors Into Memory" function
+mov al, IMAGE_SIZE ; set the sector amount
+
+mov cl, 0x02 ; set the sector position (0x02 is the first "available" sector)
+xor ch, ch ; set the cylinder (from 0x0 to 0x3ff)
+xor dh, dh ; set the head number (from 0x0 to 0xf)
+
+int 0x13 ; call the BIOS interrupt
 jmp $ ; loop forever
 
 ; constants
+VIDEO_MEMORY_SEGMENT equ 0xa000 ; segment pointing to the video memory buffer
 IMAGE_SIZE equ 125 ; 125 sectors (64000 bytes) per image
-
-; functions
-read_image:
-    pusha ; save registers
-
-    mov ah, 0x02 ; "Read Sectors Into Memory" function
-    mov al, IMAGE_SIZE ; sector amount
-
-    mov cl, 0x02 ; sector (0x02 is the first "available" sector)
-    mov ch, 0x00 ; cylinder (from 0x0 to 0x3ff)
-    mov dh, 0x00 ; head number (from 0x0 to 0xf)
-
-    int 0x13 ; call the BIOS interrupt
-
-    popa ; restore registers
-    ret ; return from function
 
 ; pad the rest of the sector with zeros
 times 510 - ($ - $$) db 0x00
