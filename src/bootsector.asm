87aaa2e487d15c5e125de7e48d9786f7300a6ac8
bemxio <bemxiov@protonmail.com>
2025-07-04 20:05:43 +0200

    write boilerplate code

--- src/bootsector.asm
+++ src/bootsector.asm
@@ -1,26 +1,35 @@
 [bits 16] ; 16-bit mode
 [org 0x7c00] ; global offset
 
-mov ax, 0x13 ; "Set Video Mode" function with 320x200 color mode
-int 0x10 ; call the BIOS interrupt
+mov ax, 0x4f02 ; "Set SVGA Video Mode" function
+mov bx, 0x10f ; 320x200 24-bit color mode
 
-mov ax, VIDEO_MEMORY_SEGMENT ; set the video memory segment
-mov es, ax ; move the segment into the extra segment register
-xor di, di ; clear the destination index
+int 0x10 ; BIOS interrupt
 
-mov ah, 0x02 ; "Read Sectors Into Memory" function
-mov al, IMAGE_SIZE ; set the sector amount
-
-mov cl, 0x02 ; set the sector position (0x02 is the first "available" sector)
-xor ch, ch ; set the cylinder (from 0x0 to 0x3ff)
-xor dh, dh ; set the head number (from 0x0 to 0xf)
-
-int 0x13 ; call the BIOS interrupt
+call display_image ; display the image data
 jmp $ ; loop forever
 
+; functions
+display_image:
+    pusha ; save registers
+
+    popa ; restore registers
+    ret ; return from function
+
+read_chunk:
+    pusha ; save registers
+
+    popa ; restore registers
+    ret ; return from function
+
+switch_bank:
+    pusha ; save registers
+
+    popa ; restore registers
+    ret ; return from function
+
 ; constants
-VIDEO_MEMORY_SEGMENT equ 0xa000 ; segment pointing to the video memory buffer
-IMAGE_SIZE equ 125 ; 125 sectors (64000 bytes) per image
+CHUNK_SIZE equ 125 ; 125 sectors (64000 bytes) per image chunk
 
 ; pad the rest of the sector with zeros
 times 510 - ($ - $$) db 0x00
